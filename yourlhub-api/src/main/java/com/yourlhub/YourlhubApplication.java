a7c02f8f8f8e71cd6a0c7651cfb777cb8320f399
YukiSato <yukiyukiamam@gmail.com>
2023-10-04 21:00:20 +0900

    [add]initial commit

---
+++ yourlhub-api/src/main/java/com/yourlhub/YourlhubApplication.java
@@ -0,0 +1,13 @@
+package com.yourlhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourlhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourlhubApplication.class, args);
+	}
+
+}
---
+++ yourlhub-api/src/test/java/com/yourlhub/YourlhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yourlhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourlhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
