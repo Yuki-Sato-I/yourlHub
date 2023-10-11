1ffd147a5a3ad582ca25ca953c22cc3048e433fe
YukiSato <yukiyukiamam@gmail.com>
2023-10-12 01:15:18 +0900

    [add]docker-compose for development

--- README.md
+++ README.md
@@ -8,3 +8,10 @@
 - java 21
 - maven 3.9.4
 - springboot 3.0.2
+
+## 開発時には
+docker composeを利用する。
+
+
+## デプロイする際には
+kubernetesを利用する。
---
+++ yourlhub-api/db/sql/postgres/initdb.d/create_database.sql
@@ -0,0 +1,5 @@
+-- Userの作成
+-- 開発環境のみで利用する.
+CREATE USER yourlhub PASSWORD 'yourlhub' CREATEDB;
+
+CREATE DATABASE yourlhub OWNER yourlhub;
